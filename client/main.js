ff52b5817046327b0a686df1b5b493817eb4faa0
suvarnak <suvarna.kadam@gmail.com>
2023-06-04 11:51:57 +0530

    updated for spoken response

--- client/main.js
+++ client/main.js
@@ -1,9 +1,13 @@
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
+const playButton = document.querySelector('#playButton');
 
 let loadInterval;
-let bot =  "fa-solid fa-robot";
-let user =  "fa-solid fa-user";
+let bot = "fa-solid fa-robot";
+let user = "fa-solid fa-user";
+let isPlaying =true;
+let pauseBtn = "fa-solid fa-pause";
+let playBtn = "fa-solid fa-play";
 
 function loader(element) {
   element.textContent = ''
@@ -35,8 +39,25 @@ function generateUniqueId() {
   return `id-${timestamp}-${hexadecimalString}`;
 }
 
+function speak(text)  {
+  window.speechSynthesis.cancel();
+
+  if (window.speechSynthesis.speaking) {
+    console.error("speechSynthesis.speaking");
+    return;
+  }
+  else {
+    console.log("start speaking....", text)
+    let speech = new SpeechSynthesisUtterance();
+    speech.text = "Chatbot says "+ text;
+    speech.lang = "en-US";
+    const utterThis = new SpeechSynthesisUtterance(speech);
+    window.speechSynthesis.speak(speech);
+  }
+}
+
 function chatStripe(isAi, value, uniqueId) {
-  isAi ? console.log("bot speaking") : console.log("user speaking....");
+  
   return (
     `
     <div class="wrapper ${isAi && 'ai'}">
@@ -45,11 +66,15 @@ function chatStripe(isAi, value, uniqueId) {
              <i class="${isAi ? bot : user}"></i>
         </div>
         <div class="message" id=${uniqueId}>${value}</div>
+        <div style="visibility: ${!isAi && 'hidden'}">
+          <i id="playButton" class="${isPlaying ? pauseBtn : playBtn}" onclick='window.speechSynthesis.pause()'></i>
+        </div>
       </div>
     </div>
     `
   )
 }
+
 
 const handleSubmit = async (e) => {
   e.preventDefault();
@@ -57,9 +82,9 @@ const handleSubmit = async (e) => {
   const data = new FormData(form);
 
   //user's chatStripe
-  chatContainer.innerHTML += chatStripe(false, data.get('prompt')); 
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
   form.reset();
- 
+
   //bot's chatStripe
   const uniqueId = generateUniqueId();
   chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
@@ -78,10 +103,11 @@ const handleSubmit = async (e) => {
   })
   clearInterval(loadInterval);
   messageDiv.innerHTML = ''; // clear the ..
-  if(response.ok) {
+  if (response.ok) {
     const data = await response.json();
     const parsedData = data.bot.trim();
     typeText(messageDiv, parsedData);
+    speak(parsedData)
   } else {
     const err = await response.text();
     messageDiv.innerHTML = "Something went wrong";
@@ -95,3 +121,5 @@ form.addEventListener('keyup', (e) => {
     handleSubmit(e);
   }
 })
+
+
