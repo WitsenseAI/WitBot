97dd091e34b50d3bd4a3521ee3836fa06387f88f
suvarnak <suvarna.kadam@gmail.com>
2023-06-09 12:32:54 +0530

    updated temp and response length

--- server/server.js
+++ server/server.js
@@ -57,8 +57,8 @@ app.post('/', async (req, res) => {
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: `${prompt}`,
-            temperature: 0.4,
-            max_tokens: 50,
+            temperature: 0.2,
+            max_tokens: 300,
             top_p: 1,
             frequency_penalty: 0.5,
             presence_penalty: 0,    
